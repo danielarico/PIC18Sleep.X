86da135f0d99e170a01a566903fe2b51503bdad2
Daniela <dani.5991@gmail.com>
2019-05-27 09:57:38 -0500

    Sleep mode working. Waking up with the comparator interrupt.

--- main.c
+++ main.c
@@ -15,6 +15,7 @@
 // Macros definition
 //*************************************************
 #define LED1 LATBbits.LATB0
+#define LED2 LATBbits.LATB1
 
 
 //*************************************************
@@ -51,29 +52,28 @@ void main(void)
 {
     OSCCONbits.IRCF = 7; // Internal Oscillator Frequency (8 MHz)
 
+    //--------------------------------------
+    // Interrupts configuration
     RCONbits.IPEN = 1; // Interrupt priority enable
     INTCONbits.PEIE = 1; // Enable low priority interrupts
     INTCONbits.GIE = 1; // Enable high priority interrupts
 
-    // Ports configuration --------------
+    //--------------------------------------
+    // Ports configuration
     TRISBbits.TRISB0 = 0; // Output
+    TRISBbits.TRISB1 = 0; // Output
+    
     LED1 = 0; // Initialize in 0
+    LED2 = 0; // Initialize in 0
 
-    // OSCCONbits.IDLEN = 0; // Sleep mode
-
-    // Comparator -----------------------
+    //--------------------------------------
+    // Comparator
+    
     // 4 inputs multiplexed to 2 comparators (CM2:CM0 = 110)
     CMCONbits.CM2 = 1;
     CMCONbits.CM1 = 1;
     CMCONbits.CM0 = 0;
     //-----------------------
-
-    CMCONbits.CIS = 0; // C1 Vin- connects to RA0; C2 Vin- to RA1
-    PIE2bits.CMIE = 1; // Enables comparator interrupts
-
-    CVRCONbits.CVREN = 1; // Comparator Voltage Reference enabled
-    CVRCONbits.CVRR = 1; // Comparator Vref range selection (low range)
-    CVRCONbits.CVRSS = 0; // Comparator ref source CVrsrc = Vdd - Vss
 
     // For CVref = 2.5V (CVR3:CVR0 = 1100 = 12)
     CVRCONbits.CVR3 = 1;
@@ -81,11 +81,25 @@ void main(void)
     CVRCONbits.CVR1 = 0;
     CVRCONbits.CVR0 = 0;
     //-----------------------
+    
+    CMCONbits.CIS = 0; // C1 Vin- connects to RA0; C2 Vin- to RA1
+    PIE2bits.CMIE = 1; // Enables comparator interrupts
+
+    CVRCONbits.CVREN = 1; // Comparator Voltage Reference enabled
+    CVRCONbits.CVRR = 1; // Comparator Vref range selection (low range)
+    CVRCONbits.CVRSS = 0; // Comparator ref source CVrsrc = Vdd - Vss
 
     CVRCONbits.CVROE = 1; // CVref voltage is also output on the RA2 pin
+    
+    // OSCCONbits.IDLEN = 0; // Sleep mode
 
     while(1)
     {
+        LED2 = 1;
+        for(int i=0; i<30000; i++){}
+        LED2 = 0;
+        for(int i=0; i<30000; i++){}
+        SLEEP();
     }
 
     return;
